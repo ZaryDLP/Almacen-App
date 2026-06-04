53090483c29c15a38777d21ce87ba2ce479c7e89
ZaryDLP <dzlpki28@gmail.com>
2026-06-04 16:13:43 -0600

    Update leer_excel.py

--- leer_excel.py
+++ leer_excel.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 # Leer el archivo Excel
-df = pd.read_excel("insumos.xlsx")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+df = pd.read_excel(os.path.join(BASE_DIR, "Insumos.xlsx"))
 
 # Mostrar todo el contenido
 print(df)
