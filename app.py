0e5f7cdaca10609ae867fa585810de5f1dd2d6f4
ZaryDLP <dzlpki28@gmail.com>
2026-06-04 16:04:18 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, request, jsonify
 import pandas as pd
-import os
 
 app = Flask(__name__)
 
 df = pd.read_excel("Insumos.xlsx")
+df.columns = df.columns.str.strip()
 
 @app.route("/")
-def index():
+def home():
     return render_template("index.html")
 
+# 🔍 API para autocomplete
 @app.route("/buscar")
 def buscar():
     q = request.args.get("q", "")
@@ -19,6 +20,11 @@ def buscar():
     ][["ESPECIFICACIÓN", "PESO UNITARIO"]]
 
     return jsonify(filtrado.to_dict(orient="records"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
